d051aae6eb47858d5660bf22028036e7abea4412
Alberto Rodríguez <rodriguezo.alberto@gmail.com>
2026-01-09 11:54:05 -0600

    shared ui tests improvements

--- tests/SharedUITests/CartesianChartTests.cs
+++ tests/SharedUITests/CartesianChartTests.cs
@@ -2,40 +2,6 @@
 using SharedUITests.Helpers;
 using Xunit;
 
-#if AVALONIA_UI_TESTING
-using ShouldLoadCartesianSUT = AvaloniaSample.General.FirstChart.View;
-using ShouldUnloadAndLoadAgainCartesianSUT = AvaloniaSample.Bars.AutoUpdate.View;
-#endif
-
-#if BLAZOR_UI_TESTING
-using ShouldLoadCartesianSUT = BlazorSample.Pages.General.FirstChart;
-#endif
-
-#if ETO_UI_TESTING
-using ShouldLoadCartesianSUT = EtoFormsSample.General.FirstChart.View;
-using ShouldUnloadAndLoadAgainCartesianSUT = EtoFormsSample.Bars.AutoUpdate.View;
-#endif
-
-#if MAUI_UI_TESTING
-using ShouldLoadCartesianSUT = MauiSample.General.FirstChart.View;
-using ShouldUnloadAndLoadAgainCartesianSUT = MauiSample.Bars.AutoUpdate.View;
-#endif
-
-#if UNO_UI_TESTING || WINUI_UI_TESTING
-using ShouldLoadCartesianSUT = WinUISample.General.FirstChart.View;
-using ShouldUnloadAndLoadAgainCartesianSUT = WinUISample.Bars.AutoUpdate.View;
-#endif
-
-#if WINFORMS_UI_TESTING
-using ShouldLoadCartesianSUT = WinFormsSample.General.FirstChart.View;
-using ShouldUnloadAndLoadAgainCartesianSUT = WinFormsSample.Bars.AutoUpdate.View;
-#endif
-
-#if WPF_UI_TESTING
-using ShouldLoadCartesianSUT = WPFSample.General.FirstChart.View;
-using ShouldUnloadAndLoadAgainCartesianSUT = WPFSample.Bars.AutoUpdate.View;
-#endif
-
 namespace SharedUITests;
 
 public class CartesianChartTests
@@ -45,7 +11,7 @@ public class CartesianChartTests
     [AppTestMethod]
     public async Task ShouldLoad()
     {
-        var sut = await App.NavigateTo<ShouldLoadCartesianSUT>();
+        var sut = await App.NavigateTo<Samples.General.FirstChart.View>();
         await sut.Chart.WaitUntilChartRenders();
 
         Assert.ChartIsLoaded(sut.Chart);
@@ -57,7 +23,7 @@ public class CartesianChartTests
     [AppTestMethod]
     public async Task ShouldUnloadAndReload()
     {
-        var sut = new ShouldUnloadAndLoadAgainCartesianSUT();
+        var sut = new Samples.Bars.AutoUpdate.View();
 
         await App.NavigateToView(sut);
         await sut.Chart.WaitUntilChartRenders();
