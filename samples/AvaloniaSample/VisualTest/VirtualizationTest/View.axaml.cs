f9c0ed28c795fce72548954c78f70641464e8057
Alberto Rodríguez <rodriguezo.alberto@gmail.com>
2026-02-09 09:54:49 -0600

    add tests for #1986

---
+++ samples/AvaloniaSample/VisualTest/VirtualizationTest/View.axaml.cs
@@ -0,0 +1,32 @@
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using LiveChartsCore.SkiaSharpView.Avalonia;
+
+namespace AvaloniaSample.VisualTest.VirtualizationTest;
+
+public partial class View : UserControl
+{
+    public View()
+    {
+        InitializeComponent();
+
+        Loaded += async (s, e) =>
+        {
+            await Task.Delay(2000);
+            OpenTab2();
+            await Task.Delay(2000);
+            ScrollToChart();
+        };
+    }
+
+    public CartesianChart Chart1 => this.Find<CartesianChart>("chart1")!;
+    public CartesianChart Chart2 => this.Find<CartesianChart>("chart2")!;
+    public void OpenTab1() => this.Find<TabControl>("tabs")!.SelectedIndex = 0;
+    public void OpenTab2() => this.Find<TabControl>("tabs")!.SelectedIndex = 1;
+    public void ScrollToChart() => ((ScrollViewer)this.Find<TabControl>("tabs")!.SelectedContent!).ScrollToEnd();
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
---
+++ tests/SharedUITests/AvaloniaTests.cs
@@ -0,0 +1,39 @@
+﻿#if AVALONIA_UI_TESTING
+
+using Factos;
+using SharedUITests.Helpers;
+using Xunit;
+
+namespace SharedUITests;
+
+public class AvaloniaTests
+{
+    public AppController App => AppController.Current;
+
+    // based on:
+    // https://github.com/Live-Charts/LiveCharts2/issues/1986
+    // ensure charts load when avalonia virtualization is on.
+    [AppTestMethod]
+    public async Task ShouldLoadTemplatedChart()
+    {
+        var sut = await App.NavigateTo<Samples.VisualTest.VirtualizationTest.View>();
+
+        // open the second tab, scroll to end and ensure the chart is loaded.
+        await Task.Delay(1000);
+        sut.OpenTab2();
+        await Task.Delay(1000);
+        sut.ScrollToChart();
+        await Task.Delay(1000);
+        Assert.ChartIsLoaded(sut.Chart2);
+
+        // now open the first tab, scroll to end and ensure the chart is loaded.
+        await Task.Delay(1000);
+        sut.OpenTab1();
+        await Task.Delay(1000);
+        sut.ScrollToChart();
+        await Task.Delay(1000);
+        Assert.ChartIsLoaded(sut.Chart1);
+    }
+}
+
+#endif
