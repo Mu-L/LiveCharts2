5fa78415635ec2dc38904435e5e78c42d584cd97
Alberto Rodríguez <rodriguezo.alberto@gmail.com>
2026-02-13 16:42:48 -0600

    add snapshot tests

---
+++ tests/SnapshotTests/Extensions.cs
@@ -0,0 +1,23 @@
+﻿using LiveChartsGeneratedCode;
+
+namespace SnapshotTests;
+
+public static class Extensions
+{
+    extension(SourceGenSKChart chart)
+    {
+        public void PointerAt(double x, double y)
+        {
+            chart.CoreChart._isPointerIn = true;
+            chart.CoreChart._isToolTipOpen = true;
+            chart.CoreChart._pointerPosition = new(x, y);
+        }
+
+        public void Snapshot(string name)
+        {
+            if (!Directory.Exists("Snapshots")) _ = Directory.CreateDirectory("Snapshots");
+            var path = Path.Combine("Snapshots", $"{name}.png");
+            chart.SaveImage(path);
+        }
+    }
+}
